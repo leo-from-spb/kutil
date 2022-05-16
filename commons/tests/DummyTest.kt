65fe4a6d67b127d1e78c6208f454a02b6173acb5
Leonid Bushuev <leonidos@gmail.com>
2022-05-16 19:41:57 +0200

    Testing infrastructure

---
+++ commons/tests/DummyTest.kt
@@ -0,0 +1,14 @@
+package lb.kutil.commons
+
+import org.junit.jupiter.api.Test
+import lb.yaka.expectations.*
+import lb.yaka.gears.*
+
+class DummyTest {
+
+    @Test
+    fun testingIsPossible() {
+      expect that 1 equalsTo 1
+    }
+
+}
